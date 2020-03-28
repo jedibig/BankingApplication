9b2108a45d8c02e11cb224003b06195e68ca0bf8
MD <MD@DESKTOP-I7ISN8A.attlocal.net>
2020-03-28 16:11:44 -0500

    These are the initials pages(login and register) and the servlet capturing the info from the register page.

--- src/com/java/controller/CreateAccountServlet.java
+++ src/com/java/controller/CreateAccountServlet.java
@@ -1,41 +1,38 @@
 package com.java.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
 /**
  * Servlet implementation class CreateAccountServlet
  */
 @WebServlet("/CreateAccountServlet")
 public class CreateAccountServlet extends HttpServlet {
+	static Logger logger = Logger.getLogger(CreateAccountServlet.class);
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CreateAccountServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		PrintWriter writer = resp.getWriter();
+		String name = req.getParameter("name");
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		Double initDeposit= Double.parseDouble(req.getParameter("initDeposit"));
+		
+		logger.info("Account created successfully");
+		
+		writer.println("<p>Account created successfuly<p>");
+		writer.println("<a href='login.html'>Go to Login</a>");
+		
 	}
 
 }
