fd16405149aedf976adf90e60ec671107261d40a
ZanyKun <nunez.kev59@gmail.com>
2020-03-28 15:15:22 -0500

    Added dto package

---
+++ src/com/java/dto/Transaction.java
@@ -0,0 +1,23 @@
+package com.java.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Data
+
+public class Transaction {
+	private int sender;
+	private int receiver;
+	private int nominal;
+}
