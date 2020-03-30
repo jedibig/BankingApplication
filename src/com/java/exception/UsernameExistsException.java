0aa2f9d7b29a27282cdc12fdb60dd5383a5d3621
ZanyKun <nunez.kev59@gmail.com>
2020-03-30 09:02:53 -0500

    Removed coding errors from certain files in the project

--- src/com/java/exception/UsernameExistsException.java
+++ src/com/java/exception/UsernameExistsException.java
@@ -1,6 +1,9 @@
 package com.java.exception;
 
 public class UsernameExistsException extends DatabaseException{
+
+	private static final long serialVersionUID = 1L;
+
 	public UsernameExistsException(String errorMessage) {
 		super(errorMessage);
 		System.out.println("The specified username already exists in our system.");
